33846c1f87d63056a064e19e28d2fd73b63e1cfc
Jesse David Goerzen <jdgoerzen@gmail.com>
2020-03-27 11:40:13 -0600

    Scripting done
    Should have added terrain and made the camera follow the player.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float speed = 1;
+	public float rotationSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Move();
+    }
+
+	void Move()
+	{
+		Turn();
+
+		float strafe = Input.GetAxis( "Horizontal" ) * Time.deltaTime * speed;
+		float forward = Input.GetAxis( "Vertical" ) * Time.deltaTime * speed;
+
+		transform.Translate(new Vector3(strafe, 0, forward));
+	}
+
+	void Turn()
+	{
+		float rotation = Input.GetAxis( "Mouse X" ) * Time.deltaTime * rotationSpeed;
+
+		transform.rotation *= Quaternion.Slerp(Quaternion.identity, Quaternion.LookRotation(rotation < 0 ? Vector3.left : Vector3.right), Mathf.Abs(rotation));
+	}
+}
