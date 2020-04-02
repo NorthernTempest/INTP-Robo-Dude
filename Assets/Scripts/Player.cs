d967e2f2adfc6868e42a0f39b1a0c312b6a302bb
Northern Tempest <jdgoerzen@gmail.com>
2020-04-01 22:46:38 -0600

    Test success
    Presentation 23:32
    Had to cut down on presentation contents. No more animation, just adding animations to the project.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public Animator anim;
+
+	public float speed = 6;
+	public float turnSpeed = .1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+		float strafe = Input.GetAxis("Horizontal");
+		float forward = Input.GetAxis("Vertical");
+
+		transform.Translate( new Vector3( strafe, 0, forward ) * speed * Time.deltaTime );
+
+		float turn = Input.GetAxis("Mouse X");
+
+		transform.rotation *= Quaternion.Slerp(Quaternion.identity, Quaternion.LookRotation(turn < 0 ? Vector3.left : Vector3.right), Mathf.Abs(turn) * turnSpeed);
+
+		anim.SetFloat( "Right", strafe );
+		anim.SetFloat( "Forward", forward );
+    }
+}
